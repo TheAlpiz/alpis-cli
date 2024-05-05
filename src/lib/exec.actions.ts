08be6881ed5cb5c18b939b42000fafa58af54c65
Alperen Ekici <110047033+TheAlpiz@users.noreply.github.com>
2024-05-05 14:24:47 +0300

    chore: Bump package version to 0.0.83 and fix project creation path issue

--- src/lib/exec.actions.ts
+++ src/lib/exec.actions.ts
@@ -1,47 +1,57 @@
-import { ExecOptions, exec } from "child_process";
+import chalk from "chalk";
+import { exec } from "child_process";
 
 function execActions(
   dependencies: string[],
   devDependencies: string[],
   projectPath: string
 ) {
-  exec(
-    `npm install ${dependencies.join(" ")} --save`,
-    { cwd: projectPath },
-    (error, stdout, stderr) => {
+  try {
+    exec(
+      `npm install ${dependencies.join(" ")} --save`,
+      { cwd: projectPath },
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error("Error installing dependencies:", error);
+          return;
+        }
+      }
+    );
+
+    exec(
+      `npm install ${devDependencies.join(" ")} --save-dev`,
+      { cwd: projectPath },
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error("Error installing dev dependencies:", error);
+          return;
+        }
+      }
+    );
+
+    exec(`npx tsc --init`, { cwd: projectPath }, (error, stdout, stderr) => {
       if (error) {
-        console.error("Error installing dependencies:", error);
+        console.error("Error initializing TypeScript:", error);
         return;
       }
-    }
-  );
-
-  exec(
-    `npm install ${devDependencies.join(" ")} --save-dev`,
-    { cwd: projectPath },
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error("Error installing dev dependencies:", error);
-        return;
-      }
-    }
-  );
-
-  exec(`npx tsc --init`, { cwd: projectPath }, (error, stdout, stderr) => {
-    if (error) {
-      console.error("Error initializing TypeScript:", error);
-      return;
-    }
-  });
+    });
+  } catch (error) {
+    chalk.red("An error occurred while executing actions:") + error.message;
+  }
 }
 
 function InitProject(projectPath: string) {
-  exec(`npm init -y`, { cwd: projectPath }, (error, stdout, stderr) => {
-    if (error) {
-      console.error("Error initializing npm:", error);
-      return;
-    }
-  });
+  try {
+    exec(`npm init -y`, { cwd: projectPath }, (error, stdout, stderr) => {
+      if (error) {
+        console.error("Error initializing npm:", error);
+        return;
+      }
+    });
+  } catch (error) {
+    chalk.red("An error occurred while initializing the project:") +
+      error.message;
+  }
 }
 
 export default execActions;
